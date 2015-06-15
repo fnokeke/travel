77427d615fbf33d53a34be9bdd55acc3f614b4b8
Fabian Okeke <fnokeke@gmail.com>
2015-06-15 14:56:01 -0700

    now dumping advanced profile but with bugs in 8 rows (less than 1% of entire data)

--- extra_db_dump.py
+++ extra_db_dump.py
@@ -1,23 +1,29 @@
 import xlrd 
 import ibm_db
+import re
 import os
 import unicodedata
+import pprint
 
 #################################################
 #               MAIN
 #################################################
 
 def format(val):
-	if type(x) == unicode: 
+
+	if type(val) == float:
+		return int(val)
+
+	if type(val) == unicode:
 		val = unicodedata.normalize('NFKD', val).encode('ascii','ignore')
-		val = val.strip('\\x')
-		if val.find("'") >= 0:
-			val = val.replace("'", '"')
 
-	else:
-		val = int(val) 
+	if val.find("'") >= 0:
+		val = val.replace("'", '"')
 
-	return val
+	# change u'' to u
+	# val = re.sub(r"''", '', val)
+	return str(val)
+
 
 if __name__ == '__main__':
 
@@ -36,29 +42,6 @@ if __name__ == '__main__':
 	# set schema 
 	ibm_db.exec_immediate(conn, "set schema = 'PROFILE'")
 
-	# drop table
-	# stmt = ibm_db.exec_immediate(conn, 
-	# 	"select tabname from syscat.tables where tabschema='PROFILE' and tabname='employee'")
-	# # if stmt:
-	# ibm_db.exec_immediate(conn, "DROP TABLE PROFILE.EMPLOYEE")
-
-	# # create table
-	# ibm_db.exec_immediate(conn, 
-	# """
-	# 	CREATE TABLE employee (firstname VARCHAR(30) NOT NULL, 
-	# 	lastname VARCHAR(30) NOT NULL, 
-	# 	email VARCHAR(50) NOT NULL, 
-	# 	id	BIGINT, 
-	# 	agency_code CHAR(5),		
-	# 	traveler_profile BIGINT,
-	# 	loyalty_type SMALLINT, 
-	# 	provider_code	CHAR(5),
-	# 	loyalty_no VARCHAR(50)
-	# 	);
-	# """
-	# )
-
-
 	# # create index
 	# ibm_db.exec_immediate(conn, 
 	# 	"CREATE INDEX I_EMPLOYEE_1 ON employee(traveler_profile);")
@@ -74,30 +57,46 @@ if __name__ == '__main__':
 
 	col_types = [type(x) for x in sheet.row_values(1)]
 
-	col_len = [str(x) for x in sheet.row_values(1)]
-	col_len = [len(x)+2 for x in col_len]
+	# col length is the length of the longest entry in the col 
+	col_len = []
+	for i in range(sheet.ncols):
+		col = sheet.col_values(i)
+		if type(col[i]) != float:
+			col = [str(x) for x in col]
+			col_max_len = max( [len(x) for x in col] )
+			# avoid max length of siz 0
+			if col_max_len == 0:
+				col_max_len = 1
+			col_len.append(col_max_len)
+		else:
+			col_len.append(15) # 15 float means that it'll be BIGINT for length
 
 	# create huge table (97 columns)
+	all_col_types = []
 	query = []
 	for i in range(len(col_names)):
 		if col_types[i] == float:
 			if col_len[i] >= 10: 
 				query.append( col_names[i] + " BIGINT" )
+				all_col_types.append("BIGINT")
 			elif col_len[i] >= 5: 
 				query.append( col_names[i] + " INTEGER" ) 
+				all_col_types.append("INTEGER")
 			elif col_len[i] >= 0: 
 				query.append( col_names[i] + " SMALLINT")
+				all_col_types.append("SMALLINT")
 		else:
 			query.append( col_names[i] + " VARCHAR(" + str(col_len[i]) + ")" )
+			all_col_types.append("VARCHAR")
 
 	tmp = ','.join(query[:-1])
 	query = "CREATE TABLE PROFILE.ADV_PROFILE (" + tmp + "," + query[-1] + ");"
 	ibm_db.exec_immediate(conn, "DROP TABLE PROFILE.ADV_PROFILE")
  	ibm_db.exec_immediate(conn, query) 
 
-	exit(1)
-	
+	# dump from excel into table
 	rows_dumped = 0
+	rows_with_bug = []
 	for i in range(1, sheet.nrows):
 		row = sheet.row_values(i)
 
@@ -105,16 +104,32 @@ if __name__ == '__main__':
 			row = [format(x) for x in row]
 		except UnicodeEncodeError: 
 			print "Unicode bug: ", row 
+			break
 
 		row = tuple(row)
-		query = "INSERT INTO EMPLOYEE VALUES" + str(row)  + ";"
+		query = "INSERT INTO ADV_PROFILE VALUES" + str(row)  + ";"
 		try:
 			ibm_db.exec_immediate(conn, query)
 		except:
-			print row
-			print "Query failed: " + query
+			# print "Query failed: "
+			# pprint.pprint(query)
+
+			rows_with_bug.append(i)
+			# print "Row No:", i 
+			
 			print ibm_db.stmt_errormsg()
+
+			# # catch the BIGINT error
+			# bigint_indices = []
+			# for index, value in enumerate(all_col_types):
+			# 	bigint_indices.append(index)
+
+			# print "Catching the bug!" 
+			# for x in bigint_indices:
+			# 	print all_col_types[x]
 		rows_dumped += 1
 
 	print "done."
 	print "Inserted", rows_dumped, "rows."
+	print "Rows with bugs: ", len(rows_with_bug)
+	print "Rows with bugs: ", rows_with_bug
