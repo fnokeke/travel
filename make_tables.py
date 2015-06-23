ac326c158f98b61edc466854bdbfa6c81b4590a1
Fabian Okeke <fnokeke@gmail.com>
2015-06-23 16:06:43 -0700

    added employee_detail table and included foreign keys for existing tables

--- make_tables.py
+++ make_tables.py
@@ -10,18 +10,18 @@ if __name__ == '__main__':
 	handler = Connect() 
 
 	######
-	#### employee table 
+	#### employee  
 	######
+	new_table = "EMPLOYEE"
 
 	# drop 
-	new_table = "EMPLOYEE"
 	if handler.table_exists(new_table):
 		query = "DROP TABLE %s" % new_table
 		handler.run_query(query)
 
 	# create
 	query = """
-		CREATE TABLE PROFILE.EMPLOYEE (
+		CREATE TABLE PROFILE.%s (
 			firstname VARCHAR(30),
 			lastname VARCHAR(30),
 			middlename VARCHAR(30),
@@ -31,63 +31,100 @@ if __name__ == '__main__':
 			email VARCHAR(50) UNIQUE NOT NULL, 
 			traveler_profile VARCHAR(30) NOT NULL,
 			CONSTRAINT pk_traveler_profile PRIMARY KEY(traveler_profile)
-		)""" 
+		)""" % new_table
 	handler.run_query(query)
 
 	# insert 
 	query = """
-		INSERT INTO PROFILE.EMPLOYEE (firstname, lastname, email, traveler_profile)
+		INSERT INTO PROFILE.%s (firstname, lastname, email, traveler_profile)
 			SELECT distinct firstname, lastname, email, traveler_profile	
 			FROM PROFILE.BIGTABLE_EMPLOYEE
-		"""
+		""" % new_table
 	handler.run_query(query)
 	print "Table '%s' successfully created." % (new_table)
 
 
 	######
-	#### employee_detail table 
+	#### employee_detail
 	######
+	new_table = "EMPLOYEE_DETAIL"
 
 	# drop 
-	new_table = "EMPLOYEE_DETAIL"
 	if handler.table_exists(new_table):
 		query = "DROP TABLE %s" % new_table
 		handler.run_query(query)
 
 	# create
 	query = """
-		CREATE TABLE PROFILE.EMPLOYEE_DETAIL (
-			email VARCHAR(50) NOT NULL, 
+		CREATE TABLE PROFILE.%s (
+			traveler_profile VARCHAR(50) NOT NULL, 
 			birthdate VARCHAR(12),
-			phone VARCHAR(30),
 			gender CHAR(1),
-			CONSTRAINT pk_email PRIMARY KEY(email)
-		)""" 
+			title CHAR(10),
+			phone_code VARCHAR(5),
+			phone VARCHAR(15),
+			work_phone_code VARCHAR(5),
+			work_phone VARCHAR(15),
+			address_1 VARCHAR(40),
+			address_2 VARCHAR(40),
+			city VARCHAR(20),
+			postalcode VARCHAR(10),
+			country VARCHAR(20),
+			CONSTRAINT fk_traveler_profile FOREIGN KEY (traveler_profile)
+			REFERENCES EMPLOYEE(traveler_profile)
+			ON DELETE RESTRICT
+		)""" % (new_table)
 	handler.run_query(query)
 
 	# insert 
 	query = """
-		INSERT INTO PROFILE.EMPLOYEE_DETAIL (email, birthdate, phone, gender)
-			SELECT distinct email, birthdate, phone, gender
+		INSERT INTO PROFILE.%s (
+				traveler_profile,
+				birthdate,
+				gender,
+				title,
+				phone_code,
+				phone,
+				work_phone_code,
+				work_phone,
+				address_1,
+				address_2,
+				city,
+				postalcode,
+				country
+		) SELECT 
+				travelerprofileid,
+				pii_birth_date,
+				pii_gender_code,
+				title,
+				phonecountrycode,
+				phonenumber,
+				work_phone_country_code,
+				work_phone,
+				address1,
+				address2,
+				city,
+				postalcode,
+				country
 			FROM PROFILE.BIGTABLE_EMPLOYEE_DETAIL
-		"""
+		""" % (new_table)
 	handler.run_query(query)
 	print "Table '%s' successfully created." % (new_table)
 
 
 	######
-	#### membership table 
+	#### membership
 	######
+	new_table = "MEMBERSHIP"
 
 	# drop 
-	new_table = "MEMBERSHIP"
 	if handler.table_exists(new_table):
 		query = "DROP TABLE %s" % new_table
 		handler.run_query(query)
 
 	# create
 	query = """
-		CREATE TABLE PROFILE.MEMBERSHIP (
+		CREATE TABLE PROFILE.%s (
 			traveler_profile VARCHAR(30),
 			id BIGINT, 
 			agency_code CHAR(5),
@@ -97,14 +134,14 @@ if __name__ == '__main__':
 			CONSTRAINT fk_traveler_profile FOREIGN KEY (traveler_profile)
 			REFERENCES EMPLOYEE(traveler_profile)
 			ON DELETE RESTRICT
-		)""" 
+		)""" % new_table
 	handler.run_query(query)
 
 	# insert 
 	query = """
-		INSERT INTO PROFILE.MEMBERSHIP (
+		INSERT INTO PROFILE.%s (
 			SELECT traveler_profile, id, agency_code, loyalty_type, provider_code, loyalty_no 
 			FROM PROFILE.BIGTABLE_EMPLOYEE
-		)"""
+		)""" % new_table
 	handler.run_query(query)
 	print "Table '%s' successfully created." % (new_table)
